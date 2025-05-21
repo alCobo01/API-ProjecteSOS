03f40f061ae0ad63f40e458b5ec8a602f0a06893
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-05-21 10:12:01 +0200

    Ingredients name table implemented

--- API-SOS_Code/DTOs/Dishes/InsertDishDTO.cs
+++ API-SOS_Code/DTOs/Dishes/InsertDishDTO.cs
@@ -5,6 +5,7 @@
         public required string Name { get; set; }
         public required string ImageUrl { get; set; }
         public required string Description { get; set; }
+        public double Price { get; set; }
         public List<string> IngredientsName { get; set; } = new List<string>();
     }
 }
