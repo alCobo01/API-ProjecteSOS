60e9a4f05ea44d992466c896d7f5cedd2a2e0ea5
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-05-22 10:34:31 +0200

    Fixed price in menu endpoint

--- API-SOS_Code/DTOs/Dishes/GetMenuDishDTO.cs
+++ API-SOS_Code/DTOs/Dishes/GetMenuDishDTO.cs
@@ -8,6 +8,7 @@ namespace API_SOS_Code.DTOs.Dishes
         public required string Name { get; set; }
         public required string Description { get; set; }
         public required string ImageUrl { get; set; }
+        public double Price { get; set; }
         public List<Ingredient> Ingredients { get; set; } = new();
     }
 }
